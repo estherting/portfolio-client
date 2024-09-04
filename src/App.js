785348602b8063794934852bd95ee5451b3819ae
Esther Ting <31228765+estherting@users.noreply.github.com>
2024-09-04 10:48:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
+  
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () =>{
+    
+    try{
+
+      const response = await api.get("/users");
+
+      // get from public api
+      //const response = await axios.get('https://bible-api.com/john 3:16');
+
+      console.log(response.data);
+
+      setMovies(response.data);
+
+    } catch(err){
+      console.log(err);
+    }
+    
+    
+  }
+
+  useEffect(() => {
+    getMovies();
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="/" element ={<Layout/>}>
+          <Route path="/" element={<Home/>} ></Route>
+        </Route>
+      </Routes>
+
     </div>
   );
 }
